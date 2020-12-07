68b485bec70037b81f6c1a5457282deeb20368ad
Polotov <aziretkul.polotov333@gmail.com>
2020-12-07 20:22:57 +0600

    new file

--- DZ/katila test.py
+++ DZ/katila test.py
@@ -1,4 +1,7 @@
-word = 'CC BB AA'
-word = word.replace('C','A',2)
-
-print(word)
+catalogue = {'hot-dot':50,'hamburger':120,'shaurma':150,'naggets':130,'piza':930,
+             'boso-lagman':250,'plov':190,'giro-lagman':240,'mantes':160,'rolls':600,
+             'garnir':100,'grechka':70,'pelmeni':130,'french meat':400,'fish':500,
+             't-bone':870,'Beijing duck':5000,'shashlyk assorti':50000
+             }
+def auth(username,password)
+    if
